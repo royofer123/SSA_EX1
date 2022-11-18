a690aa47c0e0949ffda3738918b88bbd44b03910
royofer123 <royofer123@gmail.com>
2022-11-18 14:49:15 +0200

    main fix

--- main.c
+++ main.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
-#include <math.h>
 #include "NumClass.h"
+
 int main()
 {
-    printf("%d",isStrong(149));
+    int s=0;
+    int e=0;
 
-    
+    ///printf("enter two numbers");
+    int st= scanf("%d", &s);
+    int et= scanf("%d", &e);
+    if (et!=1||st!=1||e<0||s<0) 
+    {
+    printf("UNFIXABLE USER ERROR\n");
+    printf("EXITING...\n");
+    return 1; 
+    } 
+    if (e<s) 
+    {
+        int temp = s;
+        s=e;
+        e=temp;
+    }
+
+    printf("The Armstrong numbers are:");
+    for (int i = s; i <= e; i++)
+    {
+        if (isArmstrong(i)) printf(" %d",i);
+    }
+
+    printf("\nThe Palindromes are:");
+    for (int i = s; i <= e; i++)
+    {
+        if (isPalindrom(i)) printf(" %d",i);
+    }    
+
+    printf("\nThe Prime numbers are:");
+    for (int i = s; i <= e; i++)
+    {
+        if (isPrime(i)) printf(" %d",i);
+    }    
+
+        printf("\nThe Strong numbers are:");
+    for (int i = s; i <= e; i++)
+    {
+        if (isStrong(i)) printf(" %d",i);
+    }  
+        printf("\n");
+    return 0;
 }
